f60e179e516cf0ed4a138b75d0cbb6a4d5053b20
anhta <anhta@LAPTOP-PQPMU1R4>
2023-09-18 09:21:20 +0700

    CHECK FIX

--- Buoi_1_01/src/main/java/com/example/slide1/controller/ControllerN.java
+++ Buoi_1_01/src/main/java/com/example/slide1/controller/ControllerN.java
@@ -1,9 +1,4 @@
 package com.example.slide1.controller;
-<<<<<<< HEAD
-public class ControllerN {
-=======
-
->>>>>>> branch 'master' of https://github.com/vat2003/testEclipseGithub.git
 
 public class ControllerN {
 	public void name() {
