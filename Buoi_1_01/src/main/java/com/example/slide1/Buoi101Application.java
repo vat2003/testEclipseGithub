5f35955ec7e769bfa5e2b382c102773cd5de9471
anhta <anhta@LAPTOP-PQPMU1R4>
2023-09-18 08:34:53 +0700

    First commit

---
+++ Buoi_1_01/src/main/java/com/example/slide1/Buoi101Application.java
@@ -0,0 +1,13 @@
+package com.example.slide1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buoi101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buoi101Application.class, args);
+	}
+
+}
---
+++ Buoi_1_01/src/test/java/com/example/slide1/Buoi101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slide1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
